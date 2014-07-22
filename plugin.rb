47d5de2263ff4e9d6a6c0fa2c0a6864f0843c40c
Régis Hanol <regis@hanol.fr>
2014-07-22 18:59:51 +0200

    revert debugging instructions

--- plugin.rb
+++ plugin.rb
@@ -1,32 +1,12 @@
-# name: bauth
-# about: Bloomberg OAuth
+# name: discourse-bloomberg
+# about: Bloomberg's OAuth Plugin
 # version: 0.1
 # authors: Régis Hanol
 
 require_dependency 'auth/open_id_authenticator'
 
-class BloombergAuthenticator < ::Auth::OpenIdAuthenticator
-
-  def after_authenticate(auth)
-    puts "after_authenticate"
-    puts "auth"
-    p auth
-    super
-  end
-
-  def after_create_account(user, auth)
-    puts "after_create_account"
-    puts "user"
-    p user
-    puts "auth"
-    p auth
-    super
-  end
-
-end
-
-authenticator = BloombergAuthenticator.new("bloomberg", "https://bauth.bloomberg.com/user", trusted: true)
+authenticator = ::Auth::OpenIdAuthenticator.new("bloomberg", "https://bauth.bloomberg.com/user", trusted: true)
 
 auth_provider title: "Bloomberg",
               authenticator: authenticator,
-              message: "Bloomberg message"
+              message: "Bloomberg"
